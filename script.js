bf66646bb4cff4dc3b3025ea48685c89078ece88
samixtures <samibajwa@csu.fullerton.edu>
2022-02-02 16:19:13 -0800

    Made it so all the current checkboxes can be crossed out

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let checkBox = document.querySelector("input");
-
-checkBox.addEventListener("click", function(){
-    document.querySelector("span").classList.toggle("done");
-})
+let checkBox = document.querySelectorAll("input");
+let listLength = checkBox.length;
+for (let i = 0; i < listLength; i++){
+    checkBox[i].addEventListener("click", function(){
+    document.querySelectorAll("span")[i].classList.toggle("done");
+    })
+}
+// checkBox.addEventListener("click", function(){
+//     document.querySelector("span").classList.toggle("done");
+// })
 
 
