3ea417ad96ed3b5d481cff0a8e3462803d8203d6
samixtures <samibajwa@csu.fullerton.edu>
2022-02-03 11:07:05 -0800

    Added a potentially new for loop, updated the tasks, created .vscode file to format on save using the prettier extension

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-let checkBox = document.querySelectorAll("input");
-let listLength = checkBox.length;
-for (let i = 0; i < listLength; i++){
-    checkBox[i].addEventListener("click", function(){
+let checkBoxes = document.querySelectorAll("input");
+let listLength = checkBoxes.length;
+for (let i = 0; i < listLength; i++) {
+  checkBoxes[i].addEventListener("click", function () {
     document.querySelectorAll("span")[i].classList.toggle("done");
-    })
+  });
 }
-// checkBox.addEventListener("click", function(){
-//     document.querySelector("span").classList.toggle("done");
-// })
 
-
+//Another, maybe better, way of doing the for loop for crossing out the text
+// const checkBoxLabels = document.querySelectorAll("span");
+// for (let i = 0; i < checkBoxes.length; i++) {
+//   const checkBox = checkBoxes[i];
+//   const label = checkBoxLabels[i];
+//   checkBox.addEventListener("click", () => {
+//     label.classList.toggle("done");
+//   });
+// }
